b9a2e175b492bb0d8e91c2a17b4e2e4c5b2045d2
Histman7 <hsandovalt1900@alumno.ipn.mx>
2023-02-11 22:08:35 -0600

    Cambio Procfile 2. Ajustes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hola mundo'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
