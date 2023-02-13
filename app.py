26e769d3e9ab8ea119a19ffe864750a99b6a9540
Histman7 <hsandovalt1900@alumno.ipn.mx>
2023-02-12 20:31:27 -0600

    Add config support

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
 os.environ['LANG'] = 'en_GB.UTF-8'
 os.environ['NIXPACKS_PYTHON_VERSION'] = '3.10'
+os.environ['SECRET_KEY'] = 'key-read-from-env-var'
 
 
 @app.route('/')
 def index():
-    return 'Hola mundo'
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
 
 
 if __name__ == '__main__':
